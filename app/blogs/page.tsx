2df6c0fadb5433b0aa60e9ca0b01f1f4a343ed8e
Mauricio Soares <mauricio.massayuki@gmail.com>
2026-02-06 11:17:09 -0300

    added Link and useRouter examples

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,5 +1,15 @@
-export default function Blog() {
-    return <>
-        <h1>Blog</h1>
+"use client";
+import { useRouter } from "next/navigation";
+
+const Blog = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      <h1>Blog</h1>
+      <button onClick={() => router.push("/")}>Home Page</button>
     </>
-}
+  );
+};
+
+export default Blog;

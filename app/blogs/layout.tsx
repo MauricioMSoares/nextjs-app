446610e8fb62510efb804b88a90182c4e2e50f68
Mauricio Soares <mauricio.massayuki@gmail.com>
2026-02-11 09:47:47 -0300

    added proxy example and configured layout.tsx

---
+++ app/blogs/layout.tsx
@@ -0,0 +1,8 @@
+export default function RootLayout({ children }: any) {
+  return (
+    <>
+      <h1>Blogs</h1>
+      {children}
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,22 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/blogs">Blogs</Link>
+            </li>
+            <li>
+              <Link href="/blogs/Article">Article</Link>
+            </li>
+            <li>
+              <Link href="/About">About</Link>
+            </li>
+          </ul>
+        </div>
         {children}
       </body>
     </html>
